d415b008ccd4e10b3720c90630a086c61b19c240
mizza-cz <mizza.cz666@gmail.com>
2023-03-13 10:40:45 +0100

    add static/gallery

--- src/components/aside/aside.js
+++ src/components/aside/aside.js
@@ -8,30 +8,3 @@ $(".hp-tabs-section .tab").on("click", function (event) {
     .fadeIn();
   return false;
 });
-
-$(document).ready(function () {
-  // You can override any of these options and then call...
-  $(".filterizr-filter li").click(function () {
-    var targetFilter = $(this).data("multifilter");
-    if (targetFilter === "all") {
-      $(".filterizr-filter li").removeClass("filtr-active");
-      $(this).addClass("filtr-active");
-      filterizd.filterizr("filter", "all");
-      filterizd._fltr._toggledCategories = {};
-    } else {
-      $('.filterizr-filter li[data-multifilter="all"]').removeClass(
-        "filtr-active"
-      );
-      $(this).toggleClass("filtr-active");
-      filterizd.filterizr("toggleFilter", targetFilter);
-    }
-    if (!filterizd._fltr._multifilterModeOn()) {
-      $('.filterizr-filter li[data-multifilter="all"]').addClass(
-        "filtr-active"
-      );
-    }
-  });
-
-  var filterizd = $(".filtr-container").filterizr();
-  // filterizd.filterizr('sort', 'title', 'asc');
-});
